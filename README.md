f3ebd10d560109438506a156c39a47e070d010c5
VINOD MEENA <110966446+Vinod12899@users.noreply.github.com>
2022-10-26 23:36:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# responsive-timer
+# Responsive Image Carousel Project
+
+#### Description :
+
+This web page is a project which is related to image carousel. It starts working automatically when the page is opened. Image gets changed after every time interval.
+
+#### Features of the project:
+
+- There are three parts in this project: Image Carousel, Digital Timer and Analog Timer.
+- Each of them contains a reset buttuon. And all the reset buttons are in sync. Means if any of the reset buttons is pressed then all the parts will restart. And by default time is 10 seconds.
+- After every 10 seconds, the degital timer restart from 10, the analog timer will restart from the initial position, and the image of image Carousel willl be changed to the next image automatically . 
+- There is one more feature in header section which contains an input bar and a start button.
+- Input bar can take time input from the user and the start button will assign the input to the digital timer, analog timer and image carousel as global time.
+- All the reset buttons will still be functioning.
+
+### For developers : 
+
+Functions used in this project -
+
+#### 1. updateTimer();
+
+  Updates the time of ditital timer after every second.
+
+#### 2. resetFunc();
+
+  It resets digital timer to starting time, analog timer to initial position and image carousel to first image, when any of the reset button is pressed.
+
+#### 3. rotateClock();
+
+  Creates the rotation of the hand of the analog timer.
+
+#### 4. carouselImageAnimation();
+
+  Changes the image after every time interval and creates the animation when the image gets changed.
---
+++ app.js
@@ -0,0 +1,113 @@
+const numberOfImages = 3;
+
+let startingTime = 10;
+let time = startingTime;
+let indexNav = 0;
+let indexImg = 0;
+
+const userInput = document.getElementById("entertime");
+const timer = document.getElementById("countdownTimer");
+const carouselNavDot = document.querySelectorAll(".carousel-nav-dot");
+const carouselImage = document.querySelectorAll(".carousel-image");
+const rstCarousel = document.querySelector("#carousel-reset-button");
+const rstDigital = document.querySelector(".reset");
+const arm = document.querySelector("#hand");
+const rstAnalog = document.querySelector("#reset");
+
+let timerId = setInterval(updateTimer, 1000);
+
+arm.classList.add("rotateHand");
+timer.textContent = `${time}`;
+
+start.addEventListener("click", startFunc);
+rstAnalog.addEventListener("click", resetFunc);
+rstCarousel.addEventListener("click", resetFunc);
+rstDigital.addEventListener("click", resetFunc);
+
+function updateTimer() {
+  if (parseInt(time) <= 1) {
+    time = startingTime;
+    timer.textContent = `${time}`;
+    carouselNavDotAnimation();
+    carouselImageAnimation();
+  } else {
+    time--;
+    timer.textContent = `${time}`;
+  }
+}
+
+function rotateClock() {
+  arm.style.animationDuration = userInput.value.concat("s");
+  arm.classList.remove("rotateHand");
+  void arm.offsetWidth;
+  arm.classList.add("rotateHand");
+}
+
+function startFunc() {
+  if (userInput.value === "") {
+    startingTime = 10;
+  } else {
+    startingTime = parseInt(userInput.value);
+    carouselImage.forEach((carouselImageObject) => {
+      carouselImageObject.classList.remove("current");
+      carouselImageObject.classList.remove("next");
+      carouselImageObject.style.animationDuration = userInput.value.concat("s");
+    });
+  }
+  resetFunc();
+  userInput.value = "";
+}
+
+function carouselNavDotAnimation() {
+  carouselNavDot[indexNav].classList.remove("selected");
+  indexNav++;
+  if (indexNav === numberOfImages) {
+    indexNav = 0;
+  }
+  carouselNavDot[indexNav].classList.add("selected");
+}
+
+function carouselImageAnimation() {
+  
+  carouselImage[indexImg].classList.remove("current");
+  if (indexImg == numberOfImages - 1) {
+    carouselImage[0].classList.remove("next");
+  } else {
+    carouselImage[indexImg + 1].classList.remove("next");
+  }
+
+  indexImg++;
+  if (indexImg === numberOfImages) {
+    indexImg = 0;
+  }
+
+  carouselImage[indexImg].classList.add("current");
+  if (indexImg === numberOfImages - 1) {
+    carouselImage[0].classList.add("next");
+  } else {
+    carouselImage[indexImg + 1].classList.add("next");
+  }
+}
+
+function resetFunc() {
+  clearInterval(timerId);
+  time = startingTime;
+  timer.textContent = time;
+  rotateClock();
+  timerId = setInterval(updateTimer, 1000);
+  carouselNavDot.forEach((carouselNavDotObject) => {
+    carouselNavDotObject.classList.remove("selected");
+  });
+  carouselNavDot[0].classList.add("selected");
+  indexNav = 0;
+  carouselImage.forEach((carouselImageObject) => {
+    carouselImageObject.classList.remove("current");
+    carouselImageObject.classList.remove("next");
+    void carouselImageObject.offsetWidth;
+  });
+  carouselImage[0].classList.add("current");
+  carouselImage[1].classList.add("next");
+  indexImg = 0;
+}
+
+
